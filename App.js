91b29f62dd237bea3a62c63e9bac38937d067a02
小野 純平 <snorlax.chemist.and.jazz@gmail.com>
2018-08-01 17:10:41 +0900

    ReactNativeNavigationを色々した

--- App.js
+++ App.js
@@ -1,89 +1,46 @@
-import React, {Component} from 'react';
-import {Alert, AppRegistry, FlatList, StyleSheet, Text, View, Button} from 'react-native';
-import { Navigation } from 'react-native-navigation';
+import {
+    Platform
+} from 'react-native';
+import {Navigation} from 'react-native-navigation';
 
-export default class App extends Component {
-
-    _console() {
-        console.log('hello');
-    }
-
-    _onPressButton() {
-        Alert.alert('You tapped the button!')
-        console.log('hello');
-    }
-
-    componentWillMount() {
-        this._console;
-    }
-
-    render() {
-        console.log(`[INFO] this.state.activeTab:`);
-        return (
-            <View style={styles.container}>
-                <Button
-                    onPress={this._onPressButton}
-                    title="Press Me"
-                />
-                <FlatList
-                    data={[
-                        {key: 'Devin'},
-                        {key: 'Jackson'},
-                        {key: 'James'},
-                        {key: 'Joel'},
-                        {key: 'John'},
-                        {key: 'df'},
-                        {key: 'Jun'},
-                        {key: 'Hesdfsck'},
-                        {key: 'Rin'},
-                        {key: 'Rain'},
-                        {key: 'Sara'},
-                        {key: 'Shin'},
-                        {key: 'Toma'},
-                        {key: 'Cat'},
-                        {key: 'Dr'},
-
-                    ]}
-                    renderItem={({item}) =>
-                        <View>
-                            <Text style={styles.item}
-                                  onPress={() => Alert.alert(item.key)}
-                            >{item.key}</Text>
-                        </View>
-                    }
-                />
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 22
-    },
-    item: {
-        padding: 10,
-        fontSize: 18,
-        height: 44,
-        borderStyle: 'solid',
-        borderWidth: 0.5,
-        borderColor: '#888888',
-    },
-})
-
-
-function registerScreens() {
-    Navigation.registerComponent('example.WelcomeTabScreen', () => App);
-}
+// screen related book keeping
+import {registerScreens} from './screens';
 registerScreens();
 
-Navigation.startTabBasedApp({
-    tabs: [
+const createTabs = () => {
+    let tabs = [
         {
-            label: 'Welcome',
-            screen: 'example.WelcomeTabScreen',
-            title: 'Welcome'
+            label: 'One',
+            screen: 'example.FirstTabScreen',
+            title: 'Screen One'
         },
-    ]
+        {
+            label: 'Two',
+            screen: 'example.SecondTabScreen',
+            title: 'Screen Two',
+            navigatorStyle: {
+                tabBarBackgroundColor: '#3b5998',
+            }
+        },
+        {
+            label: 'Third',
+            screen: 'example.ThirdTabScreen',
+            title: 'Screen Third',
+            navigatorStyle: {
+                tabBarBackgroundColor: '#3b5998',
+            }
+        }
+    ];
+    if (Platform.OS === 'android') {
+        tabs.push({
+            label: 'Collapsing',
+            screen: 'example.CollapsingTopBarScreen',
+            title: 'Collapsing',
+        });
+    }
+    return tabs;
+};
+// this will start our app
+Navigation.startTabBasedApp({
+    tabs: createTabs()
 });
